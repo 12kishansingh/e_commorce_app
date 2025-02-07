aaa2a800c39880ebca1ca365b5b3626132d230c9
12kishansingh <ks888singh@gmail.com>
2025-02-08 00:43:19 +0530

    added cart page and shop page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,43 @@
 import 'package:e_commorce_app/components/google_nav_bar.dart';
+import 'package:e_commorce_app/pages/cart_page.dart';
+import 'package:e_commorce_app/pages/shop_page.dart';
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // this selected index is to contorl the bottom nav bar
+  int _selectedIndex = 0;
+
+  // this method will update our selected index
+  // when user tap on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+  }
+  // pages to display
+  final List<Widget>_pages=[
+    // shop page
+    const ShopPage(),
+    // cart page
+    const CartPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      bottomNavigationBar: MyBottomNavBar(),
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],// give pages to body
     );
   }
 }
