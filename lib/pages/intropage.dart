4efb6aa7582de4075cb83ff51535de8411d889d5
12kishansingh <ks888singh@gmail.com>
2025-02-07 19:30:46 +0530

    added shop button

--- lib/pages/intropage.dart
+++ lib/pages/intropage.dart
@@ -8,39 +8,59 @@ class Intropage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       body: Center(
-        child: Column(
-          children: [
-            //logo
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 25.0,
+            vertical: 25,
+          ),
+          child: Column(
+            children: [
+              //logo
 
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Image.asset(
-                'lib/images/pic2.png',
-                height: 240,
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset(
+                  'lib/images/pic2.png',
+                  height: 240,
+                ),
               ),
-            ),
-            const SizedBox(height: 40),
-            //title
-            const Text(
-              'Share n Purchase',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+              const SizedBox(height: 30),
+              //title
+              const Text(
+                'Share n Purchase',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
               ),
-            ),
 
-            const SizedBox(height: 40),
-            //sub title
-            const Text(
-              'Sell and purchase your product here.',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
+              const SizedBox(height: 30),
+              //sub title
+              const Text(
+                'Sell and purchase your product here.you can sell what ever you want',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
               ),
-            ),
-
-            //start new button
-          ],
+              const SizedBox(height: 40),
+              //start now button
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: EdgeInsets.all(25),
+                child: Center(
+                  child: const Text(
+                    'Shop now',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
